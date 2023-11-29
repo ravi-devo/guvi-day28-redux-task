53ea57dff8aa5a63b766c369812798270fe8adf1
ravi-devo <ravi.ravichandran370@gmail.com>
2023-11-29 20:10:55 +0530

    ReadMe file included

--- src/Components/Store/store.jsx
+++ src/Components/Store/store.jsx
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import quantityReducer from "./Slices/quantitySlice";
+// import quantityReducer from "./Slices/quantitySlice";
 import cartReducer from "./Slices/cartSlice";
 
 const store = configureStore({
     reducer: {
         //name: reducerFunction
-        count: quantityReducer,
+        // count: quantityReducer,
         cart: cartReducer
     }
 });
